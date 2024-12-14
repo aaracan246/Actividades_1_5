8daf005dfff8332f88cf622fec3e64cf05ac8db1
aaracan246 <acalberto@iesrafaelalberti.es>
2024-12-14 23:19:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/actividades_1_5/screens/Actividades1_5.kt
@@ -0,0 +1,259 @@
+package com.example.actividades1_5.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
+
+/*
+Actividad 1:
+Hacer que el texto del botón muestre "Cargar perfil", pero cambie a "Cancelar"
+cuando se muestre la línea de progreso... Cuando pulsemos "Cancelar" vuelve al texto por defecto.
+*/
+@Preview(showBackground = true)
+@Composable
+fun Actividad1() {
+    var showLoading by rememberSaveable { mutableStateOf(false) }
+
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        if (showLoading) {
+            CircularProgressIndicator(
+                color = Color.Red,
+                strokeWidth = 10.dp
+            )
+            LinearProgressIndicator(
+                modifier = Modifier.padding(top = 32.dp),
+                color = Color.Red,
+                trackColor = Color.LightGray
+            )
+        }
+
+        Button(
+            onClick = { showLoading = !showLoading }
+        ) {
+            if (showLoading) {
+                Text("Cancelar")
+            }
+            else{
+                Text(text = "Cargar perfil")
+            }
+        }
+    }
+}
+
+/*
+Actividad 2:
+Modifica ahora también que se separe el botón de la línea de progreso 30 dp,
+pero usando un estado... es decir, cuando no sea visible no quiero que tenga la separación
+aunque no se vea.
+*/
+@Preview(showBackground = true)
+@Composable
+fun Actividad2() {
+    var showLoading by rememberSaveable { mutableStateOf(false) }
+
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        if (showLoading) {
+            CircularProgressIndicator(
+                color = Color.Red,
+                strokeWidth = 10.dp
+            )
+            LinearProgressIndicator(
+                modifier = Modifier.padding(top = 32.dp),
+                color = Color.Red,
+                trackColor = Color.LightGray
+            )
+        }
+
+        Button(
+            onClick = { showLoading = !showLoading },
+            modifier = Modifier.padding(if (showLoading) 30.dp else 0.dp)
+
+        ) {
+            if (showLoading) {
+                Text("Cancelar")
+            }
+            else{
+                Text(text = "Cargar perfil")
+            }
+        }
+    }
+}
+
+/*
+Actividad 3:
+- Separar los botones entre ellos 10 dp, del indicador de progreso 15 dp y centrarlos horizontalmente.
+- Cuando se clique el botón Incrementar, debe añadir 0.1 a la propiedad progress y quitar 0.1
+  cuando se pulse el botón Decrementar.
+- Evitar que nos pasemos de los márgenes de su propiedad progressStatus (0-1)
+*/
+@Composable
+fun Actividad3() {
+
+    var progress by remember { mutableFloatStateOf(0.0f) }
+    val topLimit = 1f // Limitamos por arriba la barra
+    val bottomLimit = 0f // y por abajo
+
+    Column(
+        Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LinearProgressIndicator(
+            progress = progress,
+            modifier = Modifier.padding(bottom = 15.dp))
+
+        Row(Modifier
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center) {
+            Button(
+                onClick = {
+                    if (progress < topLimit){
+                        progress += 0.1f
+                    }
+                     }) {
+                Text(text = "Incrementar")
+            }
+
+            Spacer(modifier = Modifier.padding(horizontal = 10.dp)) // Creo que esto es más preciso que meterle padding a uno de los botones en start / end
+
+            Button(onClick = {
+                if (progress > bottomLimit){
+                    progress -= 0.1f
+                }
+            }) {
+                Text(text = "Reducir")
+            }
+        }
+    }
+}
+
+
+/*
+Actividad 4:
+Sitúa el TextField en el centro de la pantalla y haz que reemplace el valor de una coma por un punto
+y que no deje escribir más de un punto decimal...
+*/
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Actividad4() {
+    var myVal by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+
+        TextField(
+            value = myVal,
+            onValueChange = { input ->
+                val changeDot = input.replace(',', '.') // Reemplazamos toda ',' por '.'
+
+                if (changeDot.count { it == '.'} <= 1){   // Contamos los caracteres '.' y lo limitamos a 1 máx
+                    myVal = changeDot
+                }
+            },
+            label = { Text(text = "Importe") }
+        )
+    }
+}
+
+
+/*
+Actividad 5:
+Haz lo mismo, pero elevando el estado a una función superior y usando un componente OutlinedTextField
+al que debes añadir un padding alrededor de 15 dp y establecer colores diferentes en los bordes
+cuando tenga el foco y no lo tenga.
+A nivel funcional no permitas que se introduzcan caracteres que invaliden un número decimal.
+*/
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Actividad5() {
+    var myVal by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Actividad5Funcionalidad(
+            value = myVal,
+            onValueChange = { myVal = it }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Actividad5Funcionalidad(value: String, onValueChange: (String) -> Unit) {
+    val focusRequester = remember { FocusRequester() }
+    var isFocused by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = { input ->
+            val changeDot = input.replace(',', '.')
+            if (changeDot.count { it == '.' } <= 1 && changeDot.isDigitsOnly()) {  // El problema de esto es que invalida el punto lo revisitaré más adelante si me da tiempo
+                onValueChange(changeDot)
+            }
+        },
+        label = { Text(text = "Importe") },
+        modifier = Modifier
+            .padding(15.dp)
+            .focusRequester(focusRequester)
+            .onFocusChanged { isFocused = it.isFocused },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Blue,
+            unfocusedBorderColor = Color.Red
+        ),
+        singleLine = true
+    )
+}
